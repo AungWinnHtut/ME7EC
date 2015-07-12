3bce53b3fa0488f9fc45459b3b418c3a78c12074
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2015-07-12 19:16:44 +0630

    add if2switch.c and del all .o and exe

---
+++ if2switch.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int iAns=0;
+	float a,r;
+	float tf,tc;
+
+	do{
+		system("cls");
+		printf("0-Exit\n");
+		printf("1-Area\n");
+		printf("2-Temp\n");
+		printf("please choose : ");
+		scanf("%d",&iAns);
+
+		switch(iAns)
+		{
+			case 0: exit(0);
+			case 1: printf("enter r "); 
+					scanf("%f",&r); 
+					a=3.14*r*r;
+					printf("\nArea is %f ",a); 
+					break;
+
+			case 2: printf("enter tf "); 
+					scanf("%f",&tf); 
+					tc=(5*(tf-32))/9;
+					printf("\ntc is %f ",tc); 
+					break;
+			default: printf("error\n"); break;
+		}
+		_getch();
+	}while(iAns!=0);
+
+
+
+	return 0;
+}
